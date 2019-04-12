26f4e9c3e2772463589dabbc6911c58bb176bf4f
Csongor Zalatnai <czalatnai@newstore.com>
2019-04-12 14:50:47 +0200

    query db, map to meaningful class

---
+++ README.md
@@ -0,0 +1,33 @@
+# Github PR metrics graphite exporter
+
+### Description
+Converts raw github PR data to time series.
+
+The data point is the number of seconds elapsed between the creation of the PR and the merging of the PR.
+
+The timestamp is the creation time of the PR.
+
+Metrics name: `prs.time_to_merge`
+
+### Example output:
+
+```
+prs.time_to_merge 3450 1554125772
+prs.time_to_merge 935617 1553187544
+```
+
+### Requirements
+Node v10+
+
+### Installation
+`npm install`
+
+### Required environment variables
+`PULL_REQUESTS_DATABASE_PATH`: path to the SQLite db file containing the PR data retrieved from GitHub. 
+
+###Usage
+```
+PULL_REQUESTS_DATABASE_PATH=sample.db node ./node_modules/ts-node/dist/bin.js index.ts
+```
+
+The generated time series will be written to `stdout`.
---
+++ index.ts
@@ -0,0 +1,42 @@
+import * as sqlite from 'sqlite';
+import * as moment from "moment";
+
+class PullRequestData {
+  differenceInSeconds: number;
+
+  constructor(private createdAt: moment.Moment, private closedAt: moment.Moment, private updatedAt: moment.Moment, private mergedAt: moment.Moment) {
+    this.differenceInSeconds = moment.duration(this.mergedAt.diff(this.createdAt)).asSeconds();
+  }
+
+  static from(createdAt: string, closedAt: string, updatedAt: string, mergedAt: string): PullRequestData {
+    return new PullRequestData(moment(createdAt), moment(closedAt), moment(updatedAt), moment(mergedAt));
+  }
+
+  toGraphite(): string {
+    return `prs.time_to_merge ${this.differenceInSeconds} ${this.createdAt.unix()}\n`;
+  }
+}
+
+async function queryDb(dbFileName: string) {
+  const db = await sqlite.open(dbFileName);
+  const results = await db.all("SELECT * FROM pull_requests");
+  return results.map((result) => {
+    const data = JSON.parse(result.data);
+    return PullRequestData.from(data.createdAt, data.closedAt, data.updatedAt, data.mergedAt);
+  })
+}
+
+if (!process.env.PULL_REQUESTS_DATABASE_PATH) {
+  console.error("Env var PULL_REQUESTS_DATABASE_PATH must be defined and" +
+      " point to the db file");
+  process.exit(1);
+}
+
+(async () => {
+  const results = await queryDb(process.env.PULL_REQUESTS_DATABASE_PATH);
+  results.forEach((result) => {
+    process.stdout.write(result.toGraphite())
+  })
+})().catch(e => {
+  console.error(e);
+});
